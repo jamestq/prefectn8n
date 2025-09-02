cf28e397bb069e6dda4def3e50efe91be5f58c38
jamestq <james.quang@pm.me>
2025-09-02 20:51:54 +1000

    :zap: fix packaging

---
+++ prefectn8n/main.py
@@ -0,0 +1,5 @@
+def app():
+    print("Hello from prefectn8n main.py!")
+
+if __name__ == "__main__":
+    app()
---
+++ prefectn8n/tasks/main.py
@@ -0,0 +1,5 @@
+def app():
+    print("Hello from main.py!")
+
+if __name__ == "__main__":  
+    app()
