769082a28510e8adb6533cf91a38e46600f49967
jamestq <james.quang@pm.me>
2025-09-02 23:56:22 +1000

    :zap: updated to enable local deployment

--- prefectn8n/main.py
+++ prefectn8n/main.py
@@ -1,11 +1,15 @@
-from prefectn8n.pipelines.preprocess import prepare_data
-from prefect import flow
+from prefectn8n.pipelines.preprocess import (
+    prepare_data,
+    save_data,
+    yet_another_flow
+)
+from prefect import serve
 
-@flow
 def app():
-    prepare_data()
+    prepare_data_deploy = prepare_data.to_deployment(name="prepare-data")
+    save_data_deploy = save_data.to_deployment(name="save-data")    
+    yet_another_flow_deploy = yet_another_flow.to_deployment(name="yet-another-flow")
+    serve(prepare_data_deploy, save_data_deploy, yet_another_flow_deploy) #type: ignore
 
 if __name__ == "__main__":
-    app.deploy(
-        name="Prepare data flow",
-    )
+    app()
--- prefectn8n/pipelines/preprocess.py
+++ prefectn8n/pipelines/preprocess.py
@@ -14,6 +14,19 @@ def prepare_data():
     df = merge_dataframes(utterances, ref_utterances, on="Line", how="inner")
     df = convert_to_str(df, "ID")
     df = fill_na(df, "Scored_Transcript", "")
+    df.head()
+
+@flow
+def save_data():
+    utterances = read_data("data/scored_utterances.csv")
+    ref_utterances = read_data("data/referenced_utterances.csv")
+    df = merge_dataframes(utterances, ref_utterances, on="Line", how="inner")
+    df = convert_to_str(df, "ID")
+    df = fill_na(df, "Scored_Transcript", "")
     df = lowercase_column(df, "Scored_Transcript")
-    df = lowercase_column(df, "Reference_Transcript")    
-    print(df.head())
+    df = lowercase_column(df, "Reference_Transcript")   
+    df.to_csv("data/cleaned_utterances.csv", index=False)
+
+@flow
+def yet_another_flow():
+    print("This is yet another flow.")
